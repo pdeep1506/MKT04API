0a93f6b8a080ac7ea26d359679ca4b4f534a95ac
pdeep1506 <pdeep1506@gmail.com>
2025-04-22 13:12:58 -0400

    implemented jwt

--- Controller/product.js
+++ Controller/product.js
@@ -1,6 +1,6 @@
 import products from '../data/products.js'
-import { v4 as uuidv4 } from 'uuid';
-
+// import { v4 as uuidv4 } from 'uuid';
+import jwt from 'jsonwebtoken';
 export const getAllProducts = (req, res) => {
     const { new: isNew, category } = req.query;
 
--- routes/product.js
+++ routes/product.js
@@ -7,10 +7,35 @@ import {
     updateProduct,
     deleteProduct
 } from '../Controller/product.js';
+import jwt from 'jsonwebtoken';
+
+
+
+const JWT_SECRET_KEY = 'your_jwt_secret_key';
+
+
+const authenticateJWT = (req, res, next) => {
+  
+  const token = req.headers['authorization']?.split(' ')[1] || req.cookies.token;
+
+  if (!token) {
+    return res.status(403).json({ message: 'Access Denied. No token provided.' });
+  }
+
+  jwt.verify(token, JWT_SECRET_KEY, (err, user) => {
+    if (err) {
+      return res.status(403).json({ message: 'Invalid token.' });
+    }
+    
+    
+    req.user = user;
+    next(); 
+  });
+};
 
 
 const router = express.Router();
-router.get("/", getAllProducts);
+router.get("/", authenticateJWT,getAllProducts);
 router.get("/:id", getProductById);
 router.post("/", createProduct);
 router.put("/:id", updateProduct);
