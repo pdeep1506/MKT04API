6a3ceff380940afc3c8d9054ac34a669547520fa
pdeep1506 <pdeep1506@gmail.com>
2025-07-15 12:31:32 -0400

    . change port

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ import cookieParser from 'cookie-parser';
 
 import productRoute from './routes/product.js'
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 8080
 
 
 app.use(cors());
